b84cacf824d818ca02b6c8a84f93399f480e9436
Alec Blakeley <alecblkly@gmail.com>
2020-03-09 18:26:31 -0400

    Insert, remove, retrieve working in hashtable. Currently not accounting for collision.

--- src/hashtable.py
+++ src/hashtable.py
@@ -1,21 +1,24 @@
 # '''
 # Linked List hash table key/value pair
 # '''
+
+
 class LinkedPair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
         self.next = None
 
+
 class HashTable:
     '''
     A hash table that with `capacity` buckets
     that accepts string keys
     '''
+
     def __init__(self, capacity):
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
-
 
     def _hash(self, key):
         '''
@@ -25,7 +28,6 @@ class HashTable:
         '''
         return hash(key)
 
-
     def _hash_djb2(self, key):
         '''
         Hash an arbitrary key using DJB2 hash
@@ -34,14 +36,12 @@ class HashTable:
         '''
         pass
 
-
     def _hash_mod(self, key):
         '''
         Take an arbitrary key and return a valid integer index
         within the storage capacity of the hash table.
         '''
         return self._hash(key) % self.capacity
-
 
     def insert(self, key, value):
         '''
@@ -51,9 +51,16 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
-
-
+        # Have key and value for this function
+        # Find the index, utilizing _hash_mod
+        index = self._hash_mod(key)
+        # Check the storage index, will print a warning
+        # TODO: LinkedPair for collision -- Day 2
+        if self.storage[index] is not None:
+            print(
+                f"Danger, Danger, Collision detected at index: ({index}).")
+        # Setting the current storage[index] to the value
+        self.storage[index] = value
 
     def remove(self, key):
         '''
@@ -63,8 +70,14 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
-
+        # Have key for this function
+        # Finding the index, utilizing _hash_mod
+        index = self._hash_mod(key)
+        # Similar to insert, if there is nothing found, print a warning
+        if self.storage[index] is None:
+            print(f"Danger, Danger, There is no key at ({index}).")
+        # If there is something found at the index, set it to none
+        self.storage[index] = None
 
     def retrieve(self, key):
         '''
@@ -74,8 +87,11 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
-
+        # Have key for this function
+        # Finding the index, utilizing _hash_mod
+        index = self._hash_mod(key)
+        # Return the index
+        return self.storage[index]
 
     def resize(self):
         '''
@@ -84,8 +100,14 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        # Only have self, similar to dynamic_array - double_size
+        # Double the capacity --> self.capacity *= 2
+        # Set a new hash table
+        # LinkedPair or Set
+        # Rehash keys
+        # Move the current over to the new
 
+        pass
 
 
 if __name__ == "__main__":
@@ -101,6 +123,10 @@ if __name__ == "__main__":
     print(ht.retrieve("line_1"))
     print(ht.retrieve("line_2"))
     print(ht.retrieve("line_3"))
+
+    # Test Removal
+    # print(ht.remove("line_1"))
+    # print(ht.remove("line_4"))
 
     # Test resizing
     old_capacity = len(ht.storage)
